1ba239240d25f8cc2f68647b1b471c4b186435b9
putra maajid <putramaajid93@gmail.com>
2023-10-16 17:50:02 +0700

    praktikum2pab

---
+++ components/Nodal.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Alert, Modal, Pressable, Image, Text, View, StyleSheet } from "react-native";
+
+const Nodal = (props) => {
+  const { modalVisible, setModalVisible, selectedItem } = props;
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        Alert.alert('Modal has been closed.');
+        setModalVisible(!modalVisible);
+      }}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          {selectedItem && (
+            <View>
+              <Image source={{ uri: selectedItem.image }} style={styles.image} />
+              <Text style={styles.text}>{selectedItem.title}</Text>
+            </View>
+          )}
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <Text style={styles.textStyle}>Hide Modal</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  image: {
+    height: 200,
+    width: null,
+  },
+  text: {
+    fontSize: 18,
+    paddingTop: 10,
+  },
+});
+
+export default Nodal;
---
+++ screens/list.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { Image, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import Nodal from '../components/Nodal';
+
+const datas = [
+  {
+    id: 1,
+    title:
+      "Dorong Pengembangan UMKM, IT Telkom Surabaya Gandeng UD Rozi dalam Pembuatan Website",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/Salinan-IMG_0823-768x512.jpg",
+  },
+  {
+    id: 2,
+    title: "Mahasiswa ITTelkom Surabaya Menang Kompetisi di New Delhi",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghjk-768x492.jpg",
+  },
+  {
+    id: 3,
+    title:
+      "Benarkah Generasi Baru Generasi Strawberi? Ayo Belajar Growth Mindset dari Kampus ITTelkom Surabaya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghkl-768x472.jpg",
+  },
+  {
+    id: 4,
+    title: "4 Industri Tahan Resesi! Pastikan Kemampuanmu Termasuk Di Dalamnya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/mbacay-768x512.jpg",
+  },
+  {
+    id: 5,
+    title:
+      "Selamat Datang Mahasiswa Baru, PKKMB ITTelkom Surabaya Berlangsung Secara Luring",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/09/DSC_1931-768x512.jpg",
+  },
+  {
+    id: 6,
+    title:
+      "Dorong Pengembangan UMKM, IT Telkom Surabaya Gandeng UD Rozi dalam Pembuatan Website",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/Salinan-IMG_0823-768x512.jpg",
+  },
+  {
+    id: 7,
+    title: "Mahasiswa ITTelkom Surabaya Menang Kompetisi di New Delhi",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghjk-768x492.jpg",
+  },
+  {
+    id: 8,
+    title:
+      "Benarkah Generasi Baru Generasi Strawberi? Ayo Belajar Growth Mindset dari Kampus ITTelkom Surabaya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghkl-768x472.jpg",
+  },
+  {
+    id: 9,
+    title: "4 Industri Tahan Resesi! Pastikan Kemampuanmu Termasuk Di Dalamnya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/mbacay-768x512.jpg",
+  },
+  {
+    id: 10,
+    title:
+      "Selamat Datang Mahasiswa Baru, PKKMB ITTelkom Surabaya Berlangsung Secara Luring",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/09/DSC_1931-768x512.jpg",
+  },
+];
+
+const List = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
+
+  return (
+    <View>
+      <View>
+        {datas.map((item) => (
+          <TouchableOpacity key={item.id} style={styles.view} onPress={() => openModal(item)}>
+            <View>
+              <Image source={{ uri: item.image }} style={styles.image} />
+              <Text style={styles.text}>{item.title}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <Nodal modalVisible={modalVisible} setModalVisible={setModalVisible} selectedItem={selectedItem} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {
+    padding: 15,
+    borderBottomColor: "#dddddd",
+    borderBottomWidth: 1,
+  },
+  image: {
+    height: 200,
+    width: null,
+  },
+  text: {
+    fontSize: 18,
+    paddingTop: 10,
+  },
+});
+
+export default List;
